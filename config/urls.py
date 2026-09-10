b179ba0a812326351f9d5f0db59bc8f112dda3e0
bidyut-kr-das <bkdas2017.bd@gmail.com>
2026-09-11 00:11:08 +0530

    fix: docker file fixed. reduced image size

--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,12 @@ from tender_search.views import (
     dashboard_home,
     dashboard_logs_view,
     gem_bid_results_view,
-    search_tender_view,
     worker_status_view,
     worker_toggle_view,
 )
 
 urlpatterns = [
     path("", dashboard_home),
-    path("api/search-tender/", search_tender_view),
     path("api/gem-bid-results/", gem_bid_results_view),
     path("api/dashboard/status/", worker_status_view),
     path("api/dashboard/worker/<str:worker>/<str:action>/", worker_toggle_view),
